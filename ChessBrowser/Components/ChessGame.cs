a740dda2de6611302849106b0f1f9d38d6d2d6f0
Ethan <u1400379@utah.edu>
2026-03-05 18:28:20 -0700

    Get Set for Chess Game

--- ChessBrowser/Components/ChessGame.cs
+++ ChessBrowser/Components/ChessGame.cs
@@ -1,14 +1,14 @@
 public class ChessGame{
     public string EventName{ get; set; }
-    public string site;
-    public string Round;
-    public string WhitePlayer;
-    public string BlackPlayer;
-    public int WhiteElo;
-    public int BlackElo;
-    public string EventDate;
-    public char Result;
-    public string moves;
+    public string site{ get; set; }
+    public string Round{ get; set; }
+    public string WhitePlayer{ get; set; }
+    public string BlackPlayer{ get; set; }
+    public int WhiteElo{ get; set; }
+    public int BlackElo{ get; set; }
+    public string EventDate{ get; set; }
+    public char Result{ get; set; }
+    public string moves{ get; set; }
 
 
 }
