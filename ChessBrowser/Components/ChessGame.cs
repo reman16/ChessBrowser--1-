96fd8f6baac06062cfda0131ca9a60c1ec9feab2
reman16 <remanadhikari16@gmail.com>
2026-03-05 18:21:13 -0700

    Added initial code to chess game

---
+++ ChessBrowser/Components/ChessGame.cs
@@ -0,0 +1,14 @@
+public class ChessGame{
+    public string EventName;
+    public string site;
+    public string Round;
+    public string WhitePlayer;
+    public string BlackPlayer;
+    public int WhiteElo;
+    public int BlackElo;
+    public string EventDate;
+    public char Result;
+    public string moves;
+
+
+}
