e274dc2fd625e2d01a93c38295a7a08e920183ce
reman16 <155710391+reman16@users.noreply.github.com>
2026-03-06 13:39:44 -0700

    updated the initial setup

--- ChessBrowser/Components/ChessGame.cs
+++ ChessBrowser/Components/ChessGame.cs
@@ -1,12 +1,12 @@
-public class ChessGame{
-    public string EventName{ get; set; }
-    public string site{ get; set; }
-    public string Round{ get; set; }
-    public string WhitePlayer{ get; set; }
-    public string BlackPlayer{ get; set; }
+public class PgnGame{
+    public string EventName{ get; set; } = "";
+    public string Site{ get; set; } = "?";
+    public string Round{ get; set; } = "";
+    public string WhitePlayer{ get; set; }= "";
+    public string BlackPlayer{ get; set; }= "";
     public int WhiteElo{ get; set; }
     public int BlackElo{ get; set; }
-    public string EventDate{ get; set; }
+    public string EventDate{ get; set; }  = "0000-00-00";
     public char Result{ get; set; }
-    public string moves{ get; set; }
+    public string Moves{ get; set; } ="";
 }
